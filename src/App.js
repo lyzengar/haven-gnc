3d8e59d714bee5737d4fc3b3d9b63665c3fdb1f5
lyzengar <darthmaul85@hotmail.com>
2019-11-12 11:14:18 -0800

    add components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
+import Nav from './components/Nav/Nav'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 
 export default App;
