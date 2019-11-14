735b5b6c40346b7f721d0005d85a5473773684ac
lyzengar <darthmaul85@hotmail.com>
2019-11-13 21:16:27 -0800

    add images, css

--- src/components/NavInner/NavInner.jsx
+++ src/components/NavInner/NavInner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './NavInner.css';
 import gnc from '../../images/gnc.png'
+import liveNation from '../../images/live-nation.png'
 
 const NavInner = (props) => {
     return (
         <div className="NavInner">
             <img src={gnc} alt="GNC logo" className="gnc-logo"/>
+            <img src={liveNation} alt="GNC logo" className="live-nation"/>
         </div>
     )
 }
